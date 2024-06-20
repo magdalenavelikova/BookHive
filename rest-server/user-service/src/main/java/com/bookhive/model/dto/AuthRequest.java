02e4aef8177aaa3f8e232ad7281a9989584b8cfa
magdalenavelikova <magibox@yahoo.com>
2024-06-20 14:29:23 +0300

    add @Valid annotation for AuthRequest

--- rest-server/user-service/src/main/java/com/bookhive/model/dto/AuthRequest.java
+++ rest-server/user-service/src/main/java/com/bookhive/model/dto/AuthRequest.java
@@ -1,5 +1,6 @@
 package com.bookhive.model.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +9,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AuthRequest {
+    @NotEmpty(message = "Username should be provided.")
     private String username;
+    @NotEmpty(message = "Password should be provided.")
     private String password;
 
 
