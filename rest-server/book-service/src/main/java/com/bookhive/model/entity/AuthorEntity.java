711f4592995180f3319882395071893349e9a5be
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-06-25 15:38:13 +0300

    Book service structure.

--- rest-server/book-service/src/main/java/com/bookhive/model/entity/AuthorEntity.java
+++ rest-server/book-service/src/main/java/com/bookhive/model/entity/AuthorEntity.java
@@ -1,6 +1,7 @@
 package com.bookhive.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 @Table(name = "authors")
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class AuthorEntity {
